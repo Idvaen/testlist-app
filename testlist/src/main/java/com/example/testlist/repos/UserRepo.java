c70d4ffaf3d0449bda527d9e145411caa175194e
Idvaen <75420852+Idvaen@users.noreply.github.com>
2021-01-29 12:08:02 +0100

    Add files via upload

---
+++ testlist/src/main/java/com/example/testlist/repos/UserRepo.java
@@ -0,0 +1,10 @@
+package com.example.testlist.repos;
+
+import com.example.testlist.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
